ba291b4dfa7add18e7edf20b2467a2279d70b98f
Manolo1987 <m.zeh.mz@gmail.com>
2024-04-16 16:19:48 +0200

    trying out new nav

---
+++ inspiration/nev_clone.vercel.app/nev-clone.vercel.app/_next/static/E30GLkFpmsmJKNQKsgoOt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/867-1df6b9a9295ee789.js","static/chunks/pages/index-1e621d6020454c37.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
